db5679cd45167e468304f8e09da10aac16853402
FredKF <fedetkd86@hotmail.com>
2021-04-11 17:56:48 +0200

    First Commit

---
+++ src/app/components/task-list/task-list.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Task } from 'src/app/models/task.model';
+
+@Component({
+  selector: 'app-task-list',
+  templateUrl: './task-list.component.html',
+  styleUrls: ['./task-list.component.css']
+})
+export class TaskListComponent {
+  list: Task[] = [];
+  constructor() { }  
+
+  addTask(ev: KeyboardEvent): void{
+    if(ev.key==="Enter"){
+      this.list.push({
+        name: (ev.target as HTMLInputElement).value, 
+        completed: false
+      });
+      (ev.target as HTMLInputElement).value = "";
+    }
+  }
+
+  toggleCompleteTask(task: Task): void{
+    task.completed = !task.completed;
+  }
+
+  deleteTask(index : number){
+    //el tercer parametro es para reemplazar algo
+    this.list.splice(index, 1);
+  }
+}
