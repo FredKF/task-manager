25e6cd551b4287cb77b8dbf70c5c26bcd196aa8d
FredKF <fedetkd86@hotmail.com>
2021-04-23 09:52:29 +0200

    Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,8 @@ import { AppComponent } from './app.component';
 import { TaskListComponent } from './components/task-list/task-list.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
 import { ListContainerComponent } from './components/task-list-container/list-container.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,9 @@ import { ListContainerComponent } from './components/task-list-container/list-co
     ListContainerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, Output,EventEmitter  } from '@angular/core';
 import { ListContainer } from 'src/app/models/list-container.model';
 import { Task } from 'src/app/models/task.model';
 
+
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
@@ -13,18 +14,25 @@ export class TaskListComponent {
   @Input() listContainer: ListContainer;
   @Input() index: number;
   task: Task;
-  visible:true;
+  visible:boolean = true;
 
   constructor() { }
 
   addNewTask(): void{
+    if(this.toggleVisible())
+    {
       this.listContainer.taskList.push({
         name : "task1",
         completed : false
       });
+    }
   }
 
   deleteList(index: number){
     this.listContainerCol.splice(index , 1);
+  }
+
+  toggleVisible(): boolean{
+    return this.visible = !this.visible;
   }
 }
