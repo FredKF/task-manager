36542d8b543430ff24ff4b9da57111224619825e
FredKF <fedetkd86@hotmail.com>
2021-04-26 22:03:15 +0200

    input import

--- src/app/components/task-list-container/list-container.component.ts
+++ src/app/components/task-list-container/list-container.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ListContainer } from 'src/app/models/list-container.model';
 
 @Component({
